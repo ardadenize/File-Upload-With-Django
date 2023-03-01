8463c1bb03eafcbf3371f9eff5be2882cbbf92fc
ardadenize <56518983+ardadenize@users.noreply.github.com>
2023-03-01 14:28:51 +0300

    Update urls.py

--- pages_projesi/pages_projesi/urls.py
+++ pages_projesi/pages_projesi/urls.py
@@ -16,14 +16,3 @@ urlpatterns = [
     path("csv/upload/", views.upload_csv.as_view(), name='upload_csv'),
     path("xlsx/upload/", views.upload_xlsx.as_view(), name='upload_xlsx'),
     path("txt/upload/", views.upload_txt.as_view(), name='upload_txt'),
-    path("docs/<int:pk>/", views.delete_csv, name= 'delete_csv'),
-    path("docs/<int:pk>/", views.delete_txt, name= 'delete_txt'),
-    path("docs/<int:pk>/", views.delete_xlsx, name= 'delete_xlsx'),
-
-    path("docs/process/<int:pk>/", views.plotcsv, name= 'process_csv'),
-    path('',include('pages.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
