88b3d5ee28471b6786e13b180296f18878dedf44
ardadenize <56518983+ardadenize@users.noreply.github.com>
2023-03-07 15:58:53 +0300

    Update urls.py

--- pages_projesi/pages_projesi/urls.py
+++ pages_projesi/pages_projesi/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from pages import views
 from django.conf.urls.static import static
 
+urlpatterns = [
     path("admin/", admin.site.urls),
     #path("upload/", views.upload, name='upload'),
     
