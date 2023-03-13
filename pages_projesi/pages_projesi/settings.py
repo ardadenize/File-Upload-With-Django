34c2d9e3f084b534a0359ccb36d35a46006078c5
ardadenize <56518983+ardadenize@users.noreply.github.com>
2023-03-13 17:47:48 +0300

    Update settings.py

--- pages_projesi/pages_projesi/settings.py
+++ pages_projesi/pages_projesi/settings.py
@@ -122,9 +122,3 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIFICIES_DIRS = [
-    os.path.join(BASE_DIR, 'pages/static')
-]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
